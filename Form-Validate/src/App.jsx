9a8c8c6d63a8fc86c87f5b4b31d8623d3cca7438
vikraman-mani <vikraman02032001@gmail.com>
2025-04-11 14:10:18 +0530

    Material UI

--- Form-Validate/src/App.jsx
+++ Form-Validate/src/App.jsx
@@ -1,8 +1,24 @@
 import "./App.css";
 import React from "react";
+import { Paper, TextField, Typography } from "@mui/material";
 
 function App() {
-  return <></>;
+  let paperStyle = {
+    width: 400,
+    margin: "20px auto",
+    padding: "20px",
+    display: "grid",
+    gap: "20px",
+  };
+
+  return (
+    <Paper elevation={20} style={paperStyle}>
+      <Typography variant="h4" align="center">
+        Create Account
+      </Typography>
+      <TextField label="name" />
+    </Paper>
+  );
 }
 
 export default App;
